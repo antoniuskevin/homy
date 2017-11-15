7e5f7fdc2e32dd84be5e3af67446c875046441ea
Kukuh Rilo Pambudi <rilokukuh@gmail.com>
2017-11-16 00:09:44 +0700

    katalog

--- app/src/main/java/com/example/antonius/homy/LoginActivity.java
+++ app/src/main/java/com/example/antonius/homy/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.antonius.homy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLogin(View view){
+        Intent register = new Intent(LoginActivity.this, LoginActivity.class);
+        startActivity(register);
+    }
+
+    public void onRegister(View view){
+        Intent register = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(register);
     }
 }
