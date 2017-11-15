76a5b271f829fd84788d752ccec83a496ca6378b
antoniuskevin <kevin.wiguna@gmail.com>
2017-11-16 01:34:45 +0700

    customer homestay info, confirmation

---
+++ app/src/main/java/com/example/antonius/homy/CustomerHomestayInfo.java
@@ -0,0 +1,13 @@
+package com.example.antonius.homy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerHomestayInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_homestay_info);
+    }
+}
